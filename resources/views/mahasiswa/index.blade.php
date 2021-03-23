a2dbae2a9c87f6aafdf6073b10cf4184be73885e
wahyush04 <wahyush04@gmail.com>
2021-03-23 20:05:52 +0800

    Altairs

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -16,29 +16,34 @@ use PhpParser\Node\Stmt\Echo_;
             </div>
             <div class="card-body">
                 <table class="table table-bordered table-striped">
-                    <tr>
-                        <th>No.</th>
-                        <th>Nama</th>
-                        <th>Email</th>
-                        <th>No. Telp</th>
-                        <th>Program Studi</th>
-                        <th>Alamat</th>
-                        <th>Aksi</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Wahyu Syarif</td>
-                        <td>Wahyush04@gmail.com</td>
-                        <td>083129679506</td>
-                        <td>S1 ILMU KOMPUTER</td>
-                        <td>Lombok Timur</td>
-                        <td>
+                    <thead>
+                        <tr>
+                            <th scope="col">No.</th>
+                            <th scope="col">Nama</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">No. Telp</th>
+                            <th scope="col">Program Studi</th>
+                            <th scope="col">Alamat</th>
+                            <th scope="col">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Wahyu Syarif</td>
+                            <td>Wahyush04@gmail.com</td>
+                            <td>083129679506</td>
+                            <td>S1 ILMU KOMPUTER</td>
+                            <td>Lombok Timur</td>
+                            <td>
 
-                            <a href="" type="button" class="btn btn-primary editbtn" data-bs-toggle="modal" data-bs-target="">Edit</a>
+                                <a href="" type="button" class="btn btn-primary editbtn" data-bs-toggle="modal" data-bs-target="">Edit</a>
 
-                            <a href="" class="btn btn-danger" onclick="return confirm('Yakin Ingin menghapus data ini?')"> Hapus </a>
-                        </td>
-                    </tr>
+                                <a href="" class="btn btn-danger" onclick="return confirm('Yakin Ingin menghapus data ini?')"> Hapus </a>
+                            </td>
+                        </tr>
+                    </tbody>
+
                 </table>
             </div>
         </div>
